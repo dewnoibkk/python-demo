7cf5d08106adcdab51f9ed4098a71446b8ca3bc5
Taipan Prasithpongchai <taipan.p@favstay.com>
2019-09-19 12:42:37 +0700

    Create condition file

---
+++ condition.py
@@ -0,0 +1,11 @@
+if 5 > 6:
+    print('5 more than 6')
+else:
+    print('6 less than 5')
+
+if 5 > 5:
+    print('5 more than 6')
+elif 5 == 5:
+    print('5 equal 5')
+else:
+    print('6 less than 5')
