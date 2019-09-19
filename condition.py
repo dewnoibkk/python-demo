48aac5244c3de5bde9ddf174468c88979d284534
Taipan Prasithpongchai <taipan.p@favstay.com>
2019-09-19 15:17:02 +0700

    Add quiz to the demo

--- condition.py
+++ condition.py
@@ -9,3 +9,14 @@ elif 5 == 5:
     print('5 equal 5')
 else:
     print('6 less than 5')
+
+
+age_input = input('Type your age: ')
+age = int(age_input)
+if age < 20:
+    print('Young')
+elif age >= 20 and age < 50:
+    print('Adult')
+else:
+    print('Senior')
+
